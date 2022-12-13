f3c3dc1f3ddf38eb8ef116b508c4c04acbf8adbe
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-12-13 09:19:44 +0530

    added overridingf

---
+++ Session-2/Session-2/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Session_2
+{
+    class A
+    {
+       public A(int x)
+        {
+            Console.WriteLine("Value of x is "+x);
+        }
+    }
+    class B:A
+    {
+       public B(int x,int y):base(x)
+        {
+            Console.WriteLine("Value of y is "+y);
+        }
+    }
+    class C:B
+    {
+       public C(int x,int y,int z):base(x,y)
+        {
+            Console.WriteLine("Value of z is " + z);
+        }
+    }
+    class Class1
+    {
+        public static void Main()
+        {
+            C c = new C(1,2,3);
+        }
+    }
+}
---
+++ Session-2/Session-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Session_2
+{
+    class baseClass
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("Hi I am in show Method in Base");
+        }
+        public void print()
+        {
+            Console.WriteLine("Hi I am in Print Method in Base");
+        }
+    }
+    class derivedClass:baseClass
+    {
+        public override void show()
+        {
+            Console.WriteLine("Hi I am in show Method in Derived");
+        }
+        public void print()
+        {
+            Console.WriteLine("Hi I am in Print Method in Derived");
+        }
+    }
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            baseClass b = new baseClass();
+            //b.show();
+            //derivedClass d = new derivedClass();
+            //d.show();
+            b = new derivedClass();
+            b.show();
+        }
+    }
+}
