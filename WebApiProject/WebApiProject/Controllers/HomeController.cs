69a167d3838804c05f7d89759c3218a3045cb598
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-12-15 10:46:37 +0530

    added ado connectio n

---
+++ WebApiProject/WebApiProject/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiProject.Models;
+
+namespace WebApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        DemoAdoDBContext db = new DemoAdoDBContext();
+        [HttpGet]
+        public IEnumerable<TblSample> Get()
+        {
+            return db.TblSamples;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] TblSample sample)
+        {
+            db.TblSamples.Add(sample);
+            db.SaveChanges();
+            return Ok("Record is being saved successfully");
+        }
+        [HttpPut]
+        public string Put([FromBody] string text)
+        {
+            return "Hi I am first Response";
+        }
+        [HttpDelete]
+        public string Delete(int id)
+        {
+            return "Hi I am first Response";
+        }
+    }
+}
---
+++ WebApiProject/WebApiProject/Models/DemoAdoDBContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace WebApiProject.Models
+{
+    public partial class DemoAdoDBContext : DbContext
+    {
+        public DemoAdoDBContext()
+        {
+        }
+
+        public DemoAdoDBContext(DbContextOptions<DemoAdoDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblSample> TblSamples { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-PP0TB7N;Initial Catalog=DemoAdoDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblSample>(entity =>
+            {
+                entity.ToTable("tblSample");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Text).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
