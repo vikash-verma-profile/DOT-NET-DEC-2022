76efcd4cf20945214825e1b16a4936d0d642045d
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-12-27 09:26:54 +0530

    added code

---
+++ Threading/Threading/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Threading;
+
+{
+    //Console.WriteLine("Hello, World!");
+    //Function1();
+    //Function2();
+
+    //created 2 threads
+    Thread obj1 = new Thread(Function1);
+    Thread obj2 = new Thread(Function2);
+    obj1.IsBackground = true;
+    //invoking these threads
+    obj1.Start();
+    obj2.Start();
+}
+static void Function1()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Function 1 exectuted " + i.ToString());
+        Thread.Sleep(4000);
+    }
+
+}
+
+static void Function2()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Function 2 exectuted " + i.ToString());
+        Thread.Sleep(4000);
+    }
+
+}
+
+
+
