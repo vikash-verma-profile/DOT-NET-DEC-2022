cd360b20f6c7a2d0f972502f43df2e7374e46723
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-12-27 10:03:56 +0530

    added

---
+++ Threading/Threading/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    /*
+   {
+    //Console.WriteLine("Hello, World!");
+    //Function1();
+    //Function2();
+
+    //created 2 threads
+    Thread obj1 = new Thread(Function1);
+    obj1.Name = "Thread 1";
+    obj1.Start();
+    //obj1.IsBackground = true;
+    Console.WriteLine("The main application has exited");
+
+    //Thread obj2 = new Thread(Function2);
+    //obj2.Name = "Thread 2";
+    //invoking these threads
+    // obj1.Start();
+    //obj2.Start();
+}
+static void Function1()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Function 1 exectuted " + i.ToString());
+        Console.ReadLine();
+        // Thread.Sleep(4000);
+    }
+
+}
+
+static void Function2()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine("Function 2 exectuted " + i.ToString());
+        Thread.Sleep(4000);
+    }
+
+}
+    */
+}
--- Threading/Threading/Program.cs
+++ Threading/Threading/Program.cs
@@ -1,38 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Threading;
-
 {
-    //Console.WriteLine("Hello, World!");
-    //Function1();
-    //Function2();
-
-    //created 2 threads
-    Thread obj1 = new Thread(Function1);
-    Thread obj2 = new Thread(Function2);
-    obj1.IsBackground = true;
-    //invoking these threads
-    obj1.Start();
-    obj2.Start();
+    Maths ObjMaths = new Maths();
+    Thread t1 = new Thread(ObjMaths.Divide);
+    t1.Start();//Child thread
+    ObjMaths.Divide();//Main thread
 }
-static void Function1()
+class Maths
 {
-    for (int i = 0; i < 10; i++)
+    public int Num1, Num2;
+    Random o = new Random();
+    public void Divide()
     {
-        Console.WriteLine("Function 1 exectuted " + i.ToString());
-        Thread.Sleep(4000);
+        for (int i = 0; i < 1000000; i++)
+        {
+            //lock (this)
+            //{
+            Monitor.Enter(this);
+                Num1 = o.Next(1, 2); //1 to 2
+                Num2 = o.Next(1, 2);//1 to 2
+                int result = Num1 / Num2;//Divide
+                Num1 = 0;
+                Num2 = 0;
+            //}
+            Monitor.Exit(this);
+        }
     }
-
 }
 
-static void Function2()
-{
-    for (int i = 0; i < 10; i++)
-    {
-        Console.WriteLine("Function 2 exectuted " + i.ToString());
-        Thread.Sleep(4000);
-    }
-
-}
 
 
 
