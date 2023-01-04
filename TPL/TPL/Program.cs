0adec509a63540fa80fb0ad6614f78989dc1da88
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-04 10:26:23 +0530

    added

---
+++ TPL/TPL/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//for vs pfor
+
+using System.Diagnostics;
+{
+    Stopwatch obj= new Stopwatch();
+    Console.WriteLine("Parallel For Loop Execution");
+    obj.Start();
+    //for (int i = 0; i < 20; i++)
+    //{
+    //    long total = DoSomeWork();
+    //    Console.WriteLine("{0} - {1}", i, total);
+    //}
+    Parallel.For(0, 20, i =>
+    {
+        long total = DoSomeWork();
+        Console.WriteLine("{0} - {1}", i, total);
+    });
+    DateTime EndDataTime=DateTime.Now;
+    Console.WriteLine("For Loop Execution end");
+    Console.WriteLine(obj.ElapsedTicks.ToString());
+    obj.Stop();
+}
+
+static long DoSomeWork()
+{
+    long total = 0;
+    for (int i = 0; i < 100000000; i++)
+    {
+        total +=i;
+    }
+    return total;
+}
