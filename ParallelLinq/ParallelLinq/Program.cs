394767bf0bcb840f83d212a7ace0b495d247f9a3
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-05 09:21:29 +0530

    added pLInq

---
+++ ParallelLinq/ParallelLinq/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ParallelLinq;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+{
+    //Console.WriteLine("Hello, World!");
+    List<Employee> employees = new List<Employee>() {
+        new Employee{ FirstName="Rakesh"},
+        new Employee{FirstName="Ramesh"},
+        new Employee{FirstName="Jem"},
+        new Employee{FirstName="John"},
+        new Employee{FirstName="Vikash"}
+    };
+    Console.WriteLine("Normal Linq");
+    Stopwatch obj=new Stopwatch();
+    obj.Start();
+    var data = from e in employees where e.FirstName.StartsWith("R") select e;
+    obj.Stop();
+    Console.WriteLine(obj.ElapsedTicks.ToString());
+    obj.Reset();
+    Console.WriteLine("After using Parallel Linq");
+    obj.Start();
+    var dataP = from e in employees.AsParallel().WithDegreeOfParallelism(2) where e.FirstName.StartsWith("R") select e;
+    obj.Stop();
+    Console.WriteLine(obj.ElapsedTicks.ToString());
+
+    foreach (var item in data)
+    {
+        Console.WriteLine(item.FirstName);
+    }
+    foreach (var item in dataP)
+    {
+        Console.WriteLine(item.FirstName);
+    }
+
+}
+
