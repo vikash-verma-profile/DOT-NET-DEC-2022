07ac7db768987a1d13bc1404ecc3f3ed353a737c
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-05 09:51:34 +0530

    added pLInq

--- ParallelLinq/ParallelLinq/Program.cs
+++ ParallelLinq/ParallelLinq/Program.cs
@@ -3,35 +3,61 @@ using ParallelLinq;
 using System.Diagnostics;
 using System.Threading.Tasks.Dataflow;
 {
-    //Console.WriteLine("Hello, World!");
-    List<Employee> employees = new List<Employee>() {
-        new Employee{ FirstName="Rakesh"},
-        new Employee{FirstName="Ramesh"},
-        new Employee{FirstName="Jem"},
-        new Employee{FirstName="John"},
-        new Employee{FirstName="Vikash"}
-    };
-    Console.WriteLine("Normal Linq");
-    Stopwatch obj=new Stopwatch();
+    //List<Employee> employees = new List<Employee>();
+    //for (int i = 0; i < 100000; i++)
+    //{
+    //    var emp = new Employee { FirstName = "Rakesh "+i };
+    //    employees.Add(emp);
+    //}
+    ////Console.WriteLine("Hello, World!");
+
+    //Console.WriteLine("Normal Linq");
+    //Stopwatch obj=new Stopwatch();
+    //obj.Start();
+    //var data = from e in employees where e.FirstName.StartsWith("R") select e;
+    //obj.Stop();
+    //Console.WriteLine(obj.ElapsedTicks.ToString());
+    //obj.Reset();
+    //Console.WriteLine("After using Parallel Linq");
+    //obj.Start();
+    //var dataP = from e in employees.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism) where e.FirstName.StartsWith("R") select e;
+    //obj.Stop();
+    //Console.WriteLine(obj.ElapsedTicks.ToString());
+
+    //foreach (var item in data)
+    //{
+    //    Console.WriteLine(item.FirstName);
+    //}
+    //foreach (var item in dataP)
+    //{
+    //    Console.WriteLine(item.FirstName);
+    //}
+
+    var numbers = Enumerable.Range(1, 20);
+    Stopwatch obj = new Stopwatch();
     obj.Start();
-    var data = from e in employees where e.FirstName.StartsWith("R") select e;
+    var evenNumbers = numbers.Where(x=>x%2==0).ToList();
     obj.Stop();
     Console.WriteLine(obj.ElapsedTicks.ToString());
     obj.Reset();
     Console.WriteLine("After using Parallel Linq");
     obj.Start();
-    var dataP = from e in employees.AsParallel().WithDegreeOfParallelism(2) where e.FirstName.StartsWith("R") select e;
+    var evenNumbersP = numbers.AsParallel().Where(x => x % 2 == 0).ToList();
     obj.Stop();
     Console.WriteLine(obj.ElapsedTicks.ToString());
-
-    foreach (var item in data)
+    foreach (var item in evenNumbers)
     {
-        Console.WriteLine(item.FirstName);
+        Console.WriteLine(item);
     }
-    foreach (var item in dataP)
+    Console.WriteLine("===========================================");
+    foreach (var item in evenNumbersP)
     {
-        Console.WriteLine(item.FirstName);
+        Console.WriteLine(item);
     }
 
+    //numbers.asParallel().Sum()
+    //numbers.asParallel().Min()
+    //numbers.asParallel().Max()
+    //numbers.asParallel().Average()
 }
 
