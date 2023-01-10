10c9ed6638e5bd40c2c111aef48e289b784f1ba8
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-10 09:16:25 +0530

    added task retry patterb

---
+++ RetryPattern/RetryPattern/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace RetryPattern
+{
+    //Retry Pattern in c#
+    internal class Program
+    {
+        static  void Main(string[] args)
+        {
+            /*
+             * RetryTimes=3
+             * WaitTime=500
+             * for()
+             * {
+             * }
+             */
+            Console.WriteLine("Main Method started");
+            RetryMethod();
+            Console.WriteLine("Main Method End");
+        }
+
+        public static async Task RetryMethod()
+        {
+            var RetryTimes = 3;
+            var WaitTime = 500;
+            for (int i = 0; i < RetryTimes; i++)
+            {
+                try
+                {
+                    await DoSomeOperation();
+                    Console.WriteLine("Operation Successful");
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Retry {i+1}: Geting Exception : {ex.Message}");
+                    await Task.Delay(WaitTime);
+                }
+            }
+        }
+        public static async Task DoSomeOperation()
+        {
+            //Doing Some Processing
+            await Task.Delay(500);
+            throw new Exception("Exception Occured while Processing/operation");
+        }
+    }
+}
