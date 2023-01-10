eb0e28c0bbd462d65256d66ce6e4d027c28511bc
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-10 11:02:17 +0530

    added task retry patterb

--- RetryPattern/RetryPattern/Class2.cs
+++ RetryPattern/RetryPattern/Class2.cs
@@ -8,24 +8,25 @@ namespace RetryPattern
 {
     internal class Class2
     {
-        public delegate void Calc(int x,int y);
-        public static void Add(int a,int b)
+        public delegate int Calc(int x,int y);
+        public static string Add(int a,int b)
         {
-            Console.WriteLine(a + "+" + b+" ="+(a+b));
+           return a + "+" + b+" ="+(a+b);
+            
         }
-        public static void Multplication(int a, int b)
+        public static string Multplication(int a, int b)
         {
-            Console.WriteLine(a + "*" + b + " =" + (a * b));
+            return a + "*" + b + " =" + (a * b);
         }
         public static void Main3()
         {
-            Calc Cal =new Calc(Add);
-            Cal += Multplication;
+            Calc Cal =Add(1);
+            Cal += Multplication(20);
            
-            Cal(1,2);
-            Console.WriteLine("===================");
-            Cal -= Add;
-            Cal(2, 2);
+            //Cal(1,2);
+            //Console.WriteLine("===================");
+            //Cal -= Add;
+            //Cal(2, 2);
 
         }
     }
