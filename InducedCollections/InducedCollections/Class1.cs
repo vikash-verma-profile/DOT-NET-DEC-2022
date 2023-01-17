79e01933f1677a3c160443ab9e34eaf739093ff0
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-17 10:33:40 +0530

    added code for latency

---
+++ InducedCollections/InducedCollections/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InducedCollections
+{
+    internal class Class1
+    { //GC.Collect(2,GCCollectionMode.Optimized);
+
+        //GCLatencyMode.LowLatency
+        ///*
+        // * 
+        //GCLatencyMode.LowLatency; Gen 0 and Gen 1
+
+        //alerts
+
+        //GCLatencyMode.SustainedLowLatency; performs on generation 0 ,1 and 2
+
+        //*/
+
+        //Console.WriteLine("Hello, World!");
+    }
+}
---
+++ InducedCollections/InducedCollections/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Runtime;
+
+namespace InducedCollections
+{
+    public class Worker
+    {
+        public int WorkedId { get; set; }
+        public List<double> DataPoints { get; set; }
+        public Worker(int workerId, List<double> dataPoints)
+        {
+            WorkedId = workerId;
+            DataPoints = dataPoints;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentLatecenyMode = GCSettings.LatencyMode;
+            GCSettings.LatencyMode = GCLatencyMode.LowLatency;
+            var watch = new Stopwatch();
+            var numberTasksToRun = 4;
+            var numberItems = 20000;
+            var random = new Random((int)DateTime.Now.Ticks);
+            var datapoints = Enumerable.Range(1,numberItems).Select(x=>random.NextDouble()).ToList();
+            var workers = new List<Worker>();
+            for (int i = 1; i <= numberTasksToRun; i++)
+            {
+                workers.Add(new Worker(i,datapoints));
+            }
+            watch.Restart();
+
+            if (workers.Any())
+            {
+                var processorcount = Environment.ProcessorCount;
+                var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = processorcount };
+                Parallel.ForEach(workers,parallelOptions, DoWork);
+            }
+            watch.Stop();
+            GCSettings.LatencyMode = currentLatecenyMode;
+            Console.WriteLine($"Time to complete in milliseconds: {watch.ElapsedMilliseconds}");
+        }
+        public static void DoWork(Worker worker)
+        {
+            Console.WriteLine($"WorkerID: {worker.WorkedId} -->new tasks with thread id : {Thread.CurrentThread.ManagedThreadId}");
+            var pos = 0;
+            foreach (var item in worker.DataPoints)
+            {
+                var subset = worker.DataPoints.Skip(pos).Take(worker.DataPoints.Count-pos).ToList();
+                pos++;
+            }
+        }
+    }
+}
