3634f342fb88203591d0966b31aa3333b8085e16
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-19 10:00:36 +0530

    added code

---
+++ ProductWebApi/ProductWebApi/Models/ProductDb1Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductWebApi.Models;
+
+public partial class ProductDb1Context : DbContext
+{
+    public ProductDb1Context()
+    {
+    }
+
+    public ProductDb1Context(DbContextOptions<ProductDb1Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
