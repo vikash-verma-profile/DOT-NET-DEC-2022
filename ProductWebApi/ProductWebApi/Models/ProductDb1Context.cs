741f3a012d37cde58735e989785f5c21257b6ed8
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-19 10:05:48 +0530

    added code

--- ProductWebApi/ProductWebApi/Models/ProductDb1Context.cs
+++ ProductWebApi/ProductWebApi/Models/ProductDb1Context.cs
@@ -15,10 +15,32 @@ public partial class ProductDb1Context : DbContext
     {
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+    public virtual DbSet<TblLogin> TblLogins { get; set; }
+
+    public virtual DbSet<TblProduct> TblProducts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-CUO3FKB;Initial Catalog=ProductDb1;Integrated Security=True;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<TblLogin>(entity =>
+        {
+            entity.ToTable("tblLogin");
+
+            entity.Property(e => e.Password).HasMaxLength(200);
+            entity.Property(e => e.UserName).HasMaxLength(200);
+        });
+
+        modelBuilder.Entity<TblProduct>(entity =>
+        {
+            entity.ToTable("tblProduct");
+
+            entity.Property(e => e.ProductDescription).HasMaxLength(200);
+            entity.Property(e => e.ProductName).HasMaxLength(200);
+        });
+
         OnModelCreatingPartial(modelBuilder);
     }
 
---
+++ ProductWebApi/ProductWebApi/Models/TblLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductWebApi.Models;
+
+public partial class TblLogin
+{
+    public int Id { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+}
