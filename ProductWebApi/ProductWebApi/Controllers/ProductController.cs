159734f0c692558803558f973639f2bfbbc7c682
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-19 10:38:40 +0530

    added code

---
+++ ProductWebApi/ProductWebApi/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductWebApi.Models;
+
+namespace ProductWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        ProductDb1Context db;
+        public ProductController(ProductDb1Context _db)
+        {
+            db = _db;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var products = await db.TblProducts.ToListAsync();
+            return Ok(products);    
+        }
+    }
+}
