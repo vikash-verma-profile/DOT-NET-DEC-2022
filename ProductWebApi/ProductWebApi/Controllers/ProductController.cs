2e0188f709e95daca637baef9359f81c45b40d7f
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-01-24 10:57:43 +0530

    added jwt auth

--- ProductWebApi/ProductWebApi/Controllers/ProductController.cs
+++ ProductWebApi/ProductWebApi/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProductWebApi.Models;
@@ -8,6 +9,7 @@ namespace ProductWebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ProductController : ControllerBase
     {
         ProductDb1Context db;
--- ProductWebApi/ProductWebApi/Program.cs
+++ ProductWebApi/ProductWebApi/Program.cs
@@ -47,7 +47,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
