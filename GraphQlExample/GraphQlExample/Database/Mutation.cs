b92d0d5f03d0d40b50b8f7b81b8ab5d64cd8a1a8
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-02-07 10:59:06 +0530

    add

--- GraphQlExample/GraphQlExample/Database/Mutation.cs
+++ GraphQlExample/GraphQlExample/Database/Mutation.cs
@@ -10,10 +10,21 @@ namespace GraphQlExample.Database
             await repository.AddAuthor(author); 
             return new AuthorPayload(author);
         }
+
+        public async Task<BookPayload> AddBook(BookInput input, [Service] Repository repository)
+        {
+
+            var author = await repository.GetAuthor(input.Author);
+            var book = new Book(Guid.NewGuid(), input.Title,author);
+            await repository.AddBook(book);
+            return new BookPayload(book);
+        }
     }
 }
 public record AuthorInput(string Name);
+public record BookInput(string Title,Guid Author);
 
 public record AuthorPayload(Author? Record,string?Error=null):Payload(Error);
+public record BookPayload(Book? Record, string? Error = null) : Payload(Error);
 
 public record Payload(string? Error);
