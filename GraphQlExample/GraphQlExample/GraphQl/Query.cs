6a790a4a7043aea298e36edc2436392b5535dba9
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-02-07 09:40:30 +0530

    add

--- GraphQlExample/GraphQlExample/GraphQl/Query.cs
+++ GraphQlExample/GraphQlExample/GraphQl/Query.cs
@@ -1,7 +1,11 @@
-﻿namespace GraphQlExample.GraphQl
+﻿using GraphQlExample.Database;
+using GraphQlExample.Models;
+
+namespace GraphQlExample.GraphQl
 {
     public class Query
     {
         //public string Hello()=>"Hello";
+        public Task<List<Book>> GetBooks([Service]Repository repository)=>repository.GetBooksAsync();    
     }
 }
