ed807c39eb4847b3b5d71ae6820d76eb0c0b2218
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-03-14 09:19:41 +0530

    added code

---
+++ RuleEngineDesignPattern/RuleEngineDesignPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace RuleEngineDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Recording
+    {
+        public enum RECORDING_VERSIONS
+        {
+            v1,
+            v2
+        }
+        private RECORDING_VERSIONS version;
+        private String Title;
+        private String Composer;
+        private String Duration;
+        private String Aritist;
+        private String Lyrics;
+    }
+}
