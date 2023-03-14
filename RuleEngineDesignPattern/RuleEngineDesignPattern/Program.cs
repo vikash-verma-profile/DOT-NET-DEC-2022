123fb696e5837456c48036e6ca8123b3938076db
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-03-14 09:25:24 +0530

    added code

--- RuleEngineDesignPattern/RuleEngineDesignPattern/Program.cs
+++ RuleEngineDesignPattern/RuleEngineDesignPattern/Program.cs
@@ -15,11 +15,32 @@
             v1,
             v2
         }
-        private RECORDING_VERSIONS version;
+        private RECORDING_VERSIONS _version;
+        public RECORDING_VERSIONS version { get; set; }
         private String Title;
         private String Composer;
         private String Duration;
         private String Aritist;
         private String Lyrics;
+    }
+    public class Validator
+    {
+        private Boolean isBankOrNull()
+        {
+
+        }
+        public List<string> ValidateRecordings(Recording recording)
+        {
+            if (recording==null)
+            {
+                return new List<string>() { "Recording was null"};
+            }
+            List<string> errors=new List<string>();
+            if (Recording.RECORDING_VERSIONS.v1.Equals(recording.version))
+            {
+
+            }
+
+        }
     }
 }
