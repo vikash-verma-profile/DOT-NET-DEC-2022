acefe6e308378ce0c39b55bbccf59378a58f3abb
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-03-28 10:15:59 +0530

    added els

---
+++ StrykerDemo/StrykerDemo.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace StrykerDemo.Tests
+{
+    public class UnitTest1
+    {
+        //[Fact]
+        //public void TestMethod1()
+        //{
+
+        //}
+    }
+}
---
+++ StrykerDemo/StrykerDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StrykerDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
